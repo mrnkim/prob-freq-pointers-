3e7c1f3d6067963eb0525fe62eebbabe5014c11d
Russell Jones <justyrones@gmail.com>
2023-04-07 17:55:36 -0600

    2 aglos done

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,25 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
-}
+"use strict";
+/* inputs: array , number
+   outputs: boolean
+  Determine if there is a pair of numbers in InputArray where the average of the pair equals the
+  target average
+**/
+function averagePair(nums, targetAvg) {
 
+  let leftIdx = 0;
+  let rightIdx = nums.length - 1;
+
+  while (leftIdx < rightIdx) {
+
+    let currentAverage = (nums[leftIdx] + nums[rightIdx]) / 2;
+
+    if (currentAverage === targetAvg) return true;
+
+    if (currentAverage > targetAvg){
+        rightIdx--;
+    } else {
+      leftIdx++;
+    }
+  }
+  return false;
+}
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,32 +1,29 @@
 "use strict";
-// add whatever parameters you deem necessary & write doc comment
-/**
- * Inputs: two strings - word, letters(all lowercased,  no space, no punctuation)
- * Output: boolean
- *
- * <Test Case>
- * 'aa', 'abc' => false
- * 'abc', 'abccc' => true
- * 'abccc', 'abc' => false
- *
- *
- * create frequency counter for both input strings - word, letters
- * looping over the word's freqeuncy counter
- *  - if key of the counter does not exist in letters counter => return false
- *  - if a count of the key is higher than the count of the same key in letters counter => returns false
- * otherwise, returns true
- */
 
+/*
+Inputs: string
+Outputs: object
+Recieves a string and creates a populated counter object
+**/
 function getCounter(str) {
-  let counter = {}; //{a:1, b:1}
+  let counter = {};
   for (let char of str) {
-    let curr = counter[char] || 0; //
+    let curr = counter[char] || 0;
     curr += 1;
     counter[char] = curr;
   }
   return counter;
 }
 
+/**
+ * Inputs: two strings - word, letters(all lowercased,  no space, no punctuation)
+ * Output: boolean
+ * create frequency counter for both input strings - word, letters
+ * looping over the word's freqeuncy counter
+ *  - if key of the counter does not exist in letters counter => return false
+ *  - if a count of the key is higher than the count of the same key in letters counter => returns false
+ * otherwise, returns true
+ */
 function canConstructWord(word, letters) {
   let wordCounter = getCounter(word);
   let lettersCounter = getCounter(letters);
